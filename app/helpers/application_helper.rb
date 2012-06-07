9c6b188e8e8ad4fb066ba3352c0553313888f1b5
jasl <jasl9187@hotmail.com>
2012-06-07 21:10:12 +0800

    updated devise views. add mongoid and devise i18n. copy some helper functions from other project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+  def parent_layout(layout)
+    @view_flow.set(:layout,output_buffer)
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
+
+  def print_error_messages resource
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count)
+
+    html = <<-HTML
+    <div class="alert alert-error">
+      <a class="close" data-dismiss="alert">x</a>
+      <strong>#{sentence}</strong>
+      <ul>
+        #{messages}
+      </ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def notice_message
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
