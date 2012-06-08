434272323a37ec885952af45fd3aa704e542b22d
jasl <jasl9187@hotmail.com>
2012-06-09 02:49:07 +0800

    update application

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test assets)))
+  Bundler.require(*Rails.groups(:assets => %w(production development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
