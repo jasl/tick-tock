56338229e33ed439cb1015c31d190dfc38ce9a8c
jasl <jasl9187@hotmail.com>
2012-06-22 03:13:38 +0800

    add generate creator part in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 require 'time'
 
-unless User.where(:email => 'demo@demo.com').exists?
+unless User.where(:email => 'jasl123@126.com').exists?
   puts "Creating the great creator..."
   User.create! :email => 'jasl123@126.com',
                :password => 'aaaaaa',
@@ -17,22 +17,19 @@ unless User.where(:email => 'demo@demo.com').exists?
 end
 demo = User.where(:email => 'demo@demo.com').first
 
-unless demo.moments.empty?
-  puts "Clearing demo user's' moments..."
-  demo.moments.destroy_all
-end
-
-puts "Creating demo moments..."
-1.upto(200) do |i|
-  moment = demo.moments.build :note_attributes => {:body => "日记#{i}"*100}
-  # just a demo, avoid any wrong :-)
-  year, month, day, time = rand(11) + 2000, rand(11) + 1, rand(28) + 1, Time.parse("#{rand(22)+1}:#{rand(58)+1}:#{rand(58)+1}")
-  # p "#{year}-#{month}-#{day} #{time.hour}:#{time.min}:#{time.sec}"
-  moment.year = year
-  moment.month = month
-  moment.day = day
-  moment.time = time
-  moment.save!
+if demo.moments.empty?
+  puts "Creating demo moments..."
+  1.upto(200) do |i|
+    moment = demo.moments.build :note_attributes => {:body => "日记#{i}"*100}
+    # just a demo, avoid any wrong :-)
+    year, month, day, time = rand(11) + 2000, rand(11) + 1, rand(28) + 1, Time.parse("#{rand(22)+1}:#{rand(58)+1}:#{rand(58)+1}")
+    # p "#{year}-#{month}-#{day} #{time.hour}:#{time.min}:#{time.sec}"
+    moment.year = year
+    moment.month = month
+    moment.day = day
+    moment.time = time
+    moment.save!
+  end
 end
 
 puts "Demo seed was successfully created."
