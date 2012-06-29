9dd63c9248025c90a2b4ea6d7a53c786ddf055df
jasl <jasl9187@hotmail.com>
2012-06-30 02:06:18 +0800

    update date format

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -14,6 +14,7 @@ class MomentsController < ApplicationController
 
     respond_to do |format|
       format.html { @page_title = t("views.moment.titles.wall") }
+      format.json { render json: @summary }
     end
   end
 
