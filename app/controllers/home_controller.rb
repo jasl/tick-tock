18df36fc42b3bf7b16ad897e81299e31ae146a09
jasl <jasl9187@hotmail.com>
2012-07-01 00:27:07 +0800

    yoooo wtf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      redirect_to moments_wall_path
+      redirect_to wall_moments_path
     else
       redirect_to about_path
     end
