26c02af2f6f117ffb491eef16e372dd8c3932dd6
jasl <jasl9187@hotmail.com>
2012-07-04 11:58:36 +0800

    add note length validate

--- app/models/embedded_models/types/note.rb
+++ app/models/embedded_models/types/note.rb
@@ -2,7 +2,7 @@ class Note < Content
 
   field :body, :type => String, :null => false, :autosave => true
 
-  validates :body, :presence => true
+  validates :body, :presence => true, :length => 10..2000
 
   embedded_in :moment, :inverse_of => :note
 
